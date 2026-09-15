e7608208f27710f004f13247fb9c6a11cc602096
Sasa Petric <skoksap5@gmail.com>
2026-09-15 18:06:20 +0000

    Fix footer padding for mobile

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -28,7 +28,7 @@ export const Footer: React.FC<FooterProps> = ({ onNavigateLandingPage, onOpenTra
   };
 
   return (
-    <footer className="bg-[#1A1512] text-[#FAF7F2] pt-16 pb-12 border-t border-white/10">
+    <footer className="bg-[#1A1512] text-[#FAF7F2] pt-16 pb-28 md:pb-12 border-t border-white/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         
         {/* Newsletter Signup Banner */}
